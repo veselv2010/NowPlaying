baab9276d57a16f61ff09f9e678fac5862861d56
Shifu462 <gunka462@gmail.com>
2018-09-24 15:43:18 +0300

    +Spotify OAuth browser window.

---
+++ NowPlaying/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace NowPlaying
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+
+            var browserWindow = new OAuth.BrowserWindow();
+            browserWindow.ShowDialog();
+
+            TextBoxToken.Text = browserWindow.ResultToken;
+
+            this.Show();
+        }
+
+        private void ButtonDo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NowPlaying/OAuth/BrowserWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Navigation;
+using mshtml;
+
+namespace NowPlaying.OAuth
+{
+    public partial class BrowserWindow : Window
+    {
+        private const string authLinkTemplate = @"https://accounts.spotify.com/en/authorize?client_id={0}&redirect_uri={1}&response_type=token&scope=user-read-playback-state";
+
+        private static string AuthLink => string.Format(authLinkTemplate, AppInfo.ClientId, AppInfo.RedirectUri);
+
+        public string ResultToken { get; private set; }
+
+        public BrowserWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Browser.Navigate(AuthLink);
+        }
+
+        private void Browser_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            // Hide scrollbar
+            (Browser.Document as IHTMLDocument2).body.parentElement.style.overflow = "hidden";
+        }
+
+        private void Browser_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            string currentUrl = e.Uri.ToString();
+            if (currentUrl.Contains("access_token="))
+            {
+                var urlParams = currentUrl.Split(new char[] { '?', '&' });
+                string token = urlParams.Single(p => p.Contains("access_token"))
+                                        .Split('=')[1]; // "access_token=*text*" split by '=', take *text*
+
+                ResultToken = token;
+
+                this.Close();
+            }
+        }
+    }
+}
