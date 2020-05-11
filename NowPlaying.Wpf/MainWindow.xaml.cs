09ab042a572c0de8fccd1a290bcc0719384a7076
Никита Веселов <veselv2010@gmail.com>
2020-05-11 16:16:13 +0300

    Теперь окно можно перетаскивать

--- NowPlaying.Wpf/MainWindow.xaml.cs
+++ NowPlaying.Wpf/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace NowPlaying.Wpf
 
         // TODO: custom colors
         private Brush ThemeToBrush(Theme theme) => theme == Theme.Black ? Brushes.DarkGray : Brushes.Gray;
+
+        private void HeaderBlock_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
     }
 }
